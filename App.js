afc58251fd8fb7301f487fff20baa41a478b0f13
Ömer Faruk <sahan.faruk.omer@outlook.com>
2022-04-27 10:17:56 +0300

    new project

--- App.js
+++ App.js
@@ -4,18 +4,36 @@ import { StyleSheet, Text, View, Button } from "react-native";
 export default function App() {
     return (
         < View style={styles.container} >
-            <Button title="Login" />
-            <Text> Open up App.js to start working on your app! </Text>
-            < StatusBar style="auto" />
-        </View>
+            <Text style={styles.world}> Hello World! </Text>
+            <Text style={styles.name}> My name is Omer</Text>
+            <Text style={styles.text}> This is my first React Native App</Text>
+            <Button style={styles.button} title="Press Me!" />
+            <StatusBar style="auto" />
+        </View >
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "red",
+        backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
+
+    },
+    world: {
+        fontSize: 36,
+        fontWeight: "bold"
+    },
+    text: {
+        color: "red",
+        fontWeight: "bold"
+    },
+    name: {
+        fontSize: 24,
+        color: "orange"
+    },
+    button: {
+        color: "red",
     },
 });
